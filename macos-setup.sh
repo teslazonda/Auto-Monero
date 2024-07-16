9e01518940ec283912f59c1063eaa91ccc22c437
Quill <69414602+teslazonda@users.noreply.github.com>
2024-07-16 21:55:13 +0900

    Add macOS script (#3)
    * Add newline at end of README
    
    * Added installed software to README
    
    * Update GitHub actions runners
    
    * Create Fedora test job
    
    * Add gitignore
    
    * Update script-test.yml
    
    * Macos (#4)
    
    * Update README.md
    
    * Update README.md
    
    * Add macOS setup script
    
    * Update dependencies list
    
    * Change directory path for building xmrig
    
    * Update macos script
    
    * Update macos script directory navigation
    
    * Update macos script

---
+++ macos-setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Auto-Monero  Copyright (C) 2024 teslazonda
+# This program comes with ABSOLUTELY NO WARRANTY;
+# This is free software, and you are welcome to redistribute it under certain conditions.
+
+cd ~ 
+echo "Create Monero-Stuff directory"
+mkdir Monero-Stuff && cd Monero-Stuff
+
+echo "Creating Monero-Stuff directory here:"
+pwd
+sleep 2
+
+# Install Homebrew
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Update all packages"
+
+sleep 2
+brew update && brew upgrade
+
+echo "Install XMRig dependencies"
+brew install cmake wget automake libtool autoconf libuv openssl hwloc
+sleep 2
+
+echo "Install xmrig"
+git clone https://github.com/xmrig/xmrig.git
+sleep 2
+
+echo "Make xmrig donation 0%"
+cd xmrig/src && sed -i 's/1/0/g' donate.h
+pwd
+
+
+echo "creating xmrig build and entering xmrig/scripts folder"
+cd ..
+cd ..
+echo "*******"
+pwd
+echo "*******"
+mkdir xmrig/build && cd xmrig/scripts
+sleep 2
+
+./build.hwloc.sh && cd ../build
+cmake .. -DOPENSSL_ROOT_DIR=$(brew --prefix openssl) -DHWLOC_INCLUDE_DIR=../scripts/deps/include -DHWLOC_LIBRARY=../scripts/deps/lib/libhwloc.a
+make -j$(sysctl -n hw.logicalcpu)
+
